248d358ab5787dd85b673a3be279fe3a09c8a455
43m04ka <89536403007r@gmail.com>
2024-11-24 12:54:43 +0300

    init

--- index.js
+++ index.js
@@ -1,18 +1,10 @@
 const TelegramBot = require('node-telegram-bot-api');
-const express = require('express');
-const cors = require('cors');
 
 
 const token = '7989552745:AAFt44LwqIMbiq75yp86zEgSJMpNxb_8BWA';
 const webAppURL  = 'https://vermillion-cobbler-e75220.netlify.app';
 
 const bot = new TelegramBot(token, {polling: true});
-const app = express();
-
-app.use(express.json());
-app.use(cors());
-
-const urlencodedParser = express.urlencoded({extended: false});
 
 bot.on('message', async (msg) => {
     const chatId = msg.chat.id;
@@ -22,7 +14,7 @@ bot.on('message', async (msg) => {
         await bot.sendMessage(chatId, 'Заходи в наш интернет магазин по кнопке ниже', {
             reply_markup: {
                 keyboard: [
-                    [{text: 'Сделать заказ', web_app: {url: webAppURL}}]
+                    [{text: 'Магазин', web_app: {url: webAppURL+'/home'}}]
                 ]
             }
         })
@@ -32,42 +24,12 @@ bot.on('message', async (msg) => {
             const data = JSON.parse(msg?.web_app_data?.data)
             console.log(data)
             setTimeout(async () => {
-                await bot.sendMessage(chatId, 'Всю информацию вы получите в этом чате');
-                await bot.sendMessage(5106439090, "что от купили")
-            }, 3000)
+                await bot.sendMessage(chatId, 'Заказ оформлен, ожидайте нашего сообщениия');
+                await bot.sendMessage(5106439090, "Что то купили")
+            }, 1000)
         } catch (e) {
             console.log(e);
         }
     }
 
 });
-
-// const corsOptions ={
-//     origin:'*',
-//     credentials:true,
-//     optionSuccessStatus:200,
-// }
-//
-// app.use(cors(corsOptions))
-
-app.post('/web-data', async (req, res) => {
-    const {queryId, products = [], totalPrice} = req.body;
-    try {
-        await bot.answerWebAppQuery(queryId, {
-            type: 'article',
-            id: queryId,
-            title: 'Успешная покупка',
-            input_message_content: {
-                message_text: ` Поздравляю с покупкой, вы приобрели товар на сумму ${totalPrice}, ${products.map(item => item.title).join(', ')}`
-            }
-        })
-        return res.status(200).json({});
-    } catch (e) {
-        return res.status(500).json({'12':'34'})
-    }
-})
-
-
-const PORT = 8000;
-
-app.listen(PORT, () => console.log('server started on PORT ' + PORT))
