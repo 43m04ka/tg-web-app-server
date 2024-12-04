20f880bad77f7434b964e5d73093e593b8ece271
43m04ka <89536403007r@gmail.com>
2024-12-04 23:59:28 +0300

    init

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const TelegramBot = require('node-telegram-bot-api');
-
+const express = require('express');
+const cors = require('cors');
 
 const token = '7989552745:AAFt44LwqIMbiq75yp86zEgSJMpNxb_8BWA';
 const webAppURL  = 'https://vermillion-cobbler-e75220.netlify.app';
 
 const bot = new TelegramBot(token, {polling: true});
+const app = express();
 
+app.use(express.json());
+app.use(cors());
+
+bot.setMyCommands([
+    { command: "/start", description: "Каталог" }
+]);
+
+console.log('123')
 bot.on('message', async (msg) => {
+    console.log(msg);
     const chatId = msg.chat.id;
     const text = msg.text;
     if(text === '/start') {
@@ -33,3 +44,24 @@ bot.on('message', async (msg) => {
     }
 
 });
+
+app.post('/web-data', async (req, res) => {
+    const {queryId, products = [], totalPrice} = req.body;
+    try {
+        await bot.answerWebAppQuery(queryId, {
+            type: 'article',
+            id: queryId,
+            title: 'Успешная покупка',
+            input_message_content: {
+                message_text: ` Поздравляю с покупкой, вы приобрели товар на сумму ${totalPrice}, ${products.map(item => item.title).join(', ')}`
+            }
+        })
+        return res.status(200).json({});
+    } catch (e) {
+        return res.status(500).json({})
+    }
+})
+
+const PORT = 8000;
+
+app.listen(PORT, () => console.log('server started on PORT ' + PORT))
