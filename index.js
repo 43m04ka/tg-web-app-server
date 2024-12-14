bf7bcad89b032468beb4510648d971ca1442045f
43m04ka <89536403007r@gmail.com>
2024-12-14 20:20:45 +0300

    init

--- index.js
+++ index.js
@@ -66,9 +66,10 @@ bot.on('message', async (msg) => {
 
 
 app.post('/basket', async (req, res) => {
-    const {method, mainData, user} = req.body;
+    const method = req.body.method;
     if(method ==='add'){
     try {
+        const {mainData, user} = req.body;
         const chatId = user.id;
         const userDb = await UserModel.findOne({chatId:chatId});
         let summa ={body :[ ...[mainData], ...userDb.basket.body]};
@@ -77,12 +78,22 @@ app.post('/basket', async (req, res) => {
         await userDb.save();
         return res.status(200).json({body: userDb.basket.body});
     } catch (e) {
-        return res.status(500).json({});
+        return res.status(501).json({});
     }}
+    else if(method ==='get'){
+        try {
+            const {user} = req.body;
+            const chatId = user.id;
+            const userDb = await UserModel.findOne({chatId: chatId});
+            return res.status(200).json({body: userDb.basket.body});
+        }catch (e) {
+            return res.status(501).json({});
+        }
+    }
 })
 
 start()
 
-app.get('/web-data', async (req, res) =>{
-    res.send({id:0, massage:'OK'})
+app.get('/basket', async (req, res) =>{
+
 });
