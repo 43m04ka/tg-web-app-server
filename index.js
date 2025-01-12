1c0215d62f5ccda1d10c8031d48a96f0608b35dc
43m04ka <89536403007r@gmail.com>
2025-01-12 12:57:35 +0300

    init

--- index.js
+++ index.js
@@ -317,8 +317,8 @@ app.post('/database', async (req, res) => {
 
                 let k = 0
                 request.map(el => {
+                    console.log(request[k].body)
                     if (typeof request[k].body.platform !== 'undefined') {
-                        console.log(request[k].body)
                         let flag = true
                         json.platform.map((platform) => {
                             console.log(flag)
