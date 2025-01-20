bfecd69f7265e2314c5061ece1a91c8ad12446c9
43m04ka <89536403007r@gmail.com>
2025-01-20 17:42:08 +0300

    init

--- index.js
+++ index.js
@@ -336,7 +336,7 @@ app.post('/database', async (req, res) => {
             return res.status(550).json({});
         }
     } else if (method === 'delCategory') {
-        const path = req.body.path.data;
+        const path = req.body.data;
         try {
             let request = []
             let len = 0
