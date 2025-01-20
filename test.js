1c93c615a81a246dc2167e1ad2dfe8a5bd96070a
43m04ka <89536403007r@gmail.com>
2025-01-20 21:59:43 +0300

    init

--- test.js
+++ test.js
@@ -1,7 +1,19 @@
-let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; //массив, можно использовать массив объектов
-let size = 3; //размер подмассива
-let subarray = []; //массив в который будет выведен результат.
-for (let i = 0; i <Math.ceil(array.length/size); i++){
-    subarray[i] = array.slice((i*size), (i*size) + size);
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let randomArray = []
+
+count = 0
+while(count<10) {
+    let randomItem = array[Math.floor(Math.random() * array.length)];
+    let add = true
+    randomArray.map(el=>{
+        if(el === randomItem){
+            add = false
+        }
+    })
+    if(add){
+        randomArray= [...randomArray, randomItem]
+        count++
+    }
 }
-console.log(subarray);
+
+console.log(randomArray);
