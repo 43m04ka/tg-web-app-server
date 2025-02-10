078b788a12ec0fe0f7dbcba73c4d1b52da440dfb
43m04ka <89536403007r@gmail.com>
2025-02-10 20:05:18 +0300

    init

--- models.js
+++ models.js
@@ -4,7 +4,7 @@ const {DataTypes} = require('sequelize');
 const Users = sequelize.define('users', {
     id: {type: DataTypes.INTEGER, primaryKey: true, unique: true, autoIncrement: true},
     chatId: {type: DataTypes.STRING, unique: true},
-    basket: {type: DataTypes.RANGE, defaultValue: []},
+    basket: {type: DataTypes.ARRAY(DataTypes.INTEGER), defaultValue: []},
 })
 
 const Data = sequelize.define('data', {
