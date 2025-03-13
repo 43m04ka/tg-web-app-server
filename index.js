4a9edc2a130b21430f343f5664d67219797a7e6c
43m04ka <89536403007r@gmail.com>
2025-03-13 03:12:20 +0300

    init

--- index.js
+++ index.js
@@ -1282,12 +1282,12 @@ setInterval(async () => {
 
             StructureData1[cat.tab].body[1] = newArray
 
-            console.log(StructureData1)
-
-            // const dataDb = await DataModel.findOne({id: 1});
-            // dataDb.body = StructureData1;
-            // await dataDb.save();
-            // await reload()
+            if(typeof StructureData1 !== 'undefined') {
+                const dataDb = await DataModel.findOne({id: 1});
+                dataDb.body = StructureData1;
+                await dataDb.save();
+                await reload()
+            }
 
             console.log(cat.path, new Date(cat.deleteData))
         }
